659d98e76890a779ae2e6b08b676d470e35e795d
Matthew Cornell <matt@matthewcornell.org>
2015-02-09 12:07:14 -0500

    WIP getting there

--- forecast/Sticker.py
+++ forecast/Sticker.py
@@ -5,19 +5,20 @@ class Sticker:
     def __init__(self, forecast):
         """
         :return: an instance whose image property is a PIL Image corresponding to forecast to be used for sticker embedding
+        todo think: del(draw)
         """
         self.forecast = forecast
         self.tableSize = (126, 187)     # excluding brand but including row and column headers
         self.brandSize = (self.tableSize[0], 20)
         self.imageSize = self.tableSize[0] + self.brandSize[0], self.tableSize[1] + self.brandSize[1]
         self.image = Image.new('RGB', self.imageSize, 'white')
-        self.drawSquaresRowColHeadings()
-        self.drawGrid()
+        self.drawSquaresRowColHeadsGrid()
         self.drawBrand()
+        self.drawOverallOutline()
 
         # todo for now a temporary png file so we have an Image to work with:
         # self.image = Image.open('/Users/matt/IdeaProjects/rc-weather-flask/app/static/sticker-126x187-temp.png')
-        
+
 
     def drawColumnHeadings(self,squareSize):
         print('drawColumnHeadings()')
@@ -26,17 +27,16 @@ class Sticker:
             print('  drawColumnHeadings(): x,y={},{}. h={}'.format(x, y, colHeader))
             draw = ImageDraw.Draw(self.image)
             draw.text((x, y), colHeader, 'black')
-            # del(draw)
 
 
-    def drawSquaresRowColHeadings(self):
+    def drawSquaresRowColHeadsGrid(self):
         # for now all squares, including row and column headers, are equal sizes
         firstHour, numHours = 8, 13  # 8a to 8p
         hoursAsCalendarRows = self.forecast.hoursAsCalendarRows()
         numCols = len(hoursAsCalendarRows[0]) + 1   # including row header
         # excluding brand and column header
         squareSize = self.tableSize[0] / numCols, (self.tableSize[1] - self.brandSize[1]) / (numHours + 1)    # including column header
-        print('drawSquaresRowColHeadings(): ts={}, bs={} | nc={}, nr={}, ss={}'.format(self.tableSize, self.brandSize, numCols, range(numHours), squareSize))
+        print('drawSquaresRowColHeadsGrid(): ts={}, bs={} | nc={}, nr={}, ss={}'.format(self.tableSize, self.brandSize, numCols, range(numHours), squareSize))
         self.drawColumnHeadings(squareSize)
         for rowNum in range(numHours):  # hour of day rows. excludes column header
             # x, y = upper left corner. y skips brand height and column header row
@@ -50,13 +50,13 @@ class Sticker:
                 x += squareSize[0]
                 cssClass = hour.cssClassForDesirability()
                 self.drawHourSquare(x, y, squareSize, cssClass)
+        self.drawGrid(squareSize, numCols, numHours)
 
 
     def drawRowHeading(self, x, y, rowHeading, rowHeadingColor):
         print('  drawRowHeading(): x,y={},{}. rh,rhc={},{}'.format(x, y, rowHeading, rowHeadingColor))
         draw = ImageDraw.Draw(self.image)
         draw.text((x, y), rowHeading, rowHeadingColor)
-        # del(draw)
 
 
     def drawHourSquare(self, x, y, squareSize, cssClass):
@@ -71,13 +71,37 @@ class Sticker:
         print('    drawHourSquare(): x,y={},{}. hc={}'.format(x, y, hourColor))
         draw = ImageDraw.Draw(self.image)
         draw.rectangle((x, y, x + squareSize[0], y + squareSize[1]), hourColor)
-        # del(draw)
 
 
-
-    def drawGrid(self):
-        pass
+    def drawGrid(self, squareSize, numCols, numRows):
+        print('drawGrid(): nc,nr={},{}'.format(numCols, numRows))
+        draw = ImageDraw.Draw(self.image)
+        # draw horiz lines. include extra above column headers
+        lineColor = 'gray'
+        for rowNum in range(numRows + 1):
+            x1, y1 = 0, self.brandSize[1] + (squareSize[1] * rowNum)
+            x2 = self.tableSize[0]
+            print('  drawGrid() horiz: rn={}, x1,y1={},{}. x2={}'.format(rowNum, x1, y1, x2))
+            draw.line([x1, y1, x2, y1], lineColor)
+        # draw vertical lines. include extra left of row headers
+        for colNum in range(numCols + 1):
+            x1, y1 = squareSize[0] * colNum, self.brandSize[1]
+            y2 = self.tableSize[1]
+            print('  drawGrid() vert: cn={}, x1,y1={},{}. y2={}'.format(colNum, x1, y1, y2))
+            draw.line([x1, y1, x1, y2], lineColor)
 
 
     def drawBrand(self):
+        # todo Q: use existing brand image?
+        x, y = 0, 0
+        brandText = "PeepWeather.com"
+        print('drawBrand(): x,y={},{}. h={}'.format(x, y, brandText))
+        # self.brandSize[1]
+        draw = ImageDraw.Draw(self.image)
+        draw.text((x, y), brandText, 'black')
+
+
+    def drawOverallOutline(self):
+        # light gray around entire table
         pass
+    
