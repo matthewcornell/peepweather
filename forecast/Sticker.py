bd3632e452f7291472da1e1590a52776b2831d21
Matthew Cornell <matt@matthewcornell.org>
2015-02-08 22:57:30 -0500

    WIP

--- forecast/Sticker.py
+++ forecast/Sticker.py
@@ -9,9 +9,11 @@ class Sticker:
         self.forecast = forecast
         self.tableSize = (126, 187)
         self.brandSize = (self.tableSize[0], 20)
-        self.image = Image.new('RGB', (self.tableSize[0] + self.brandSize[0], self.tableSize[1] + self.brandSize[1]))
-        self.drawSquares()
+        self.imageSize = self.tableSize[0] + self.brandSize[0], self.tableSize[1] + self.brandSize[1]
+        self.image = Image.new('RGB', self.imageSize)
+        self.drawRowHeadings()
         self.drawColumnHeadings()
+        self.drawSquares()
         self.drawGrid()
         self.drawBrand()
 
@@ -19,7 +21,48 @@ class Sticker:
         self.image = Image.open('/Users/matt/IdeaProjects/rc-weather-flask/app/static/sticker-126x187-temp.png')
 
 
+    def drawRowHeadings(self):
+        pass
+
+
+    def drawColumnHeadings(self):
+        pass
+
+
     def drawSquares(self):
+        # for now all squares, including row and column headers, are equal sizes
+        firstHour, numHours = 8, 13  # 8a to 8p
+        hoursAsCalendarRows = self.forecast.hoursAsCalendarRows()
+        numCols = len(hoursAsCalendarRows[0])
+        squareSize = self.tableSize[0] / numCols, self.tableSize[1] / numHours  # AKA numRows
+        print('xx')
+        for hourNum in range(numHours):
+            hourOfDayIndex = hourNum + firstHour
+            x, y = squareSize[0] * 1, self.brandSize[1] + squareSize[1] * 1  # upper left corner, x skipping row header column, y skipping brand height and column header row
+            hourOfDayRow = hoursAsCalendarRows[hourOfDayIndex]
+            rowHeadingColor = 'black' if self.forecast.isDaylightHour(hourOfDayRow[0]) else 'gray'  # todo cleaner if css classes
+            rowHeading = self.forecast.rowHeadingForHour(hourOfDayIndex)  # '8', '12p', etc.
+            print('  idx={}, rhc={}, rh={}'.format(hourOfDayIndex, rowHeadingColor, rowHeading))
+            self.drawRowHeading(x, y, squareSize, rowHeading, rowHeadingColor)
+            for hour in hourOfDayRow:
+                cssClass = hour.cssClassForDesirability()
+                print('    css={}, hc={} -- hr={}'.format(cssClass, cssClass, hour))
+                self.drawHour(x, y, squareSize, cssClass)
+
+
+    def drawGrid(self):
+        pass
+
+
+    def drawBrand(self):
+        pass
+
+
+    def drawRowHeading(self, x, y, squareSize, rowHeading, rowHeadingColor):
+        pass
+
+
+    def drawHour(self, x, y, squareSize, cssClass):
         cssClassToColor = {
             'Poor': '#ff0000',
             'Fair': '#ffaa00',
@@ -27,29 +70,5 @@ class Sticker:
             'Great': '#00ff00',
             'Missing': 'white',  # ffffff
         }  # todo should probably grab this from hour-colors.css in case it changes
-        hoursAsCalendarRows = self.forecast.hoursAsCalendarRows()
-        print('xx')
-        for hourOfDayIndex in range(8, 21):
-            hourOfDayRow = hoursAsCalendarRows[hourOfDayIndex]
-            hour0 = hourOfDayRow[0]
-            rowHeadingColor = 'black' if self.forecast.isDaylightHour(hour0) else 'gray'  # todo cleaner if css classes
-            rowHeading = self.forecast.rowHeadingForHour(hourOfDayIndex)  # 8, 9, ..., 12p, 1p, ... 8p
-            print('  idx={}, rhc={}, rh={}'.format(hourOfDayIndex, rowHeadingColor, rowHeading))
-            for hour in hourOfDayRow:
-                cssClass = hour.cssClassForDesirability()
-                hourColor = cssClassToColor[cssClass]
-                print('    hr={}. css={}, hc={}'.format(hour, cssClass, hourColor))
-                # todo!
-
-
-    def drawColumnHeadings(self):
-        headerRow = self.forecast.calendarHeaderRow()
-        # todo!
-
-
-    def drawGrid(self):
-        pass  # todo!
-
-
-    def drawBrand(self):
-        pass  # todo!
+        hourColor = cssClassToColor[cssClass]
+        pass
